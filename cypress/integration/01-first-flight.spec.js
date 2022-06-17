13bcfa5f5a0b14c7b4c6c11525f1dca4274bf305
laurapacis <laural@tuta.io>
2022-06-17 12:58:18 +0200

    setup and write first test

--- cypress/integration/01-first-flight.spec.js
+++ cypress/integration/01-first-flight.spec.js
@@ -1,3 +1,17 @@
 /// <reference types="cypress" />
 
-describe('Create a New Item', () => {});
+// from Cypress docs – example of a failing test:
+// describe('My First Test', () => { // Set up the application state
+//   it('Does not do much!', () => { // Take an action
+//     expect(true).to.equal(true) // Make an assertion about the resulting application state
+//   })
+// })
+
+describe('Create a New Item', () => {
+    beforeEach(() => { // beforeEach runs before EACH test (oc xD)
+        cy.visit('/jetsetter')
+    });
+
+    it('should', () => {})
+});
+
