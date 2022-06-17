6c32da27ba634cbffd081d3c105f7b7a6510aa06
laurapacis <laural@tuta.io>
2022-06-17 20:01:16 +0200

    basic practice: adding new element + filtering items

--- cypress/integration/02-basic-practice.spec.js
+++ cypress/integration/02-basic-practice.spec.js
@@ -6,17 +6,46 @@ describe('Basic Practice', () => {
   });
 
   describe('Adding a new item', () => {
-    it('should put a new item on the page after clicking on "Add Item"', () => {});
+    it('should put a new item on the page after clicking on "Add Item"', () => {
+      const item = 'good attitude'; // declare it because your brain wont work at the end of the day
+      
+      cy.get('[data-test="new-item-input"]').type(item); // write new item
+      cy.get('[data-test="add-item"]').click(); // add new item
 
-    it('should put a new item in the "Unpacked Items" list', () => {});
+      cy.contains(item); // check if new item is added
+    });
 
-    it('should put a new item as the last item in the "Unpacked Items" list', () => {});
+    it('should put a new item in the "Unpacked Items" list', () => {
+      const item = 'good attitude';
+
+      cy.get('[data-test="new-item-input"]').type(item);
+      cy.get('form').submit(); // instead of .click() which also actually submits the form
+      cy.get('[data-test="items-unpacked"]').contains(item);
+    });
+
+    it('should put a new item as the last item in the "Unpacked Items" list', () => {
+      const item = 'good attitude'; 
+
+      cy.get('[data-test="new-item-input"]').type(item);
+      cy.get('form').submit();
+
+      cy.get('[data-test="items-unpacked"] li').last().contains(item);
+    });
   });
 
   describe('Filtering items', () => {
-    it('should show items that match whatever is in the filter field', () => {});
+    it('should show items that match whatever is in the filter field', () => {
+      cy.get('[data-test="filter-items"]').type('Tooth');
 
-    it('should hide items that do not match whatever is in the filter field', () => {});
+      cy.get('[data-test="items-unpacked"]').contains('Tooth Brush');
+      cy.get('[data-test="items-unpacked"]').contains('Tooth Paste');
+    });
+
+    it.only('should hide items that do not match whatever is in the filter field', () => {
+      cy.get('[data-test="filter-items"]').type('Tooth');
+
+      cy.contains('booo').should('not.exist');
+    });
   });
 
   describe('Removing items', () => {
